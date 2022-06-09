a93e49e8ede5d3bb7aa1bce8b2d3058807c25f72
leticia-ribeiro98 <107193074+leticia-ribeiro98@users.noreply.github.com>
2022-06-09 12:08:29 -0300

    Update Prova 5.1.py

--- Prova 5.1.py
+++ Prova 5.1.py
@@ -1,3 +1,10 @@
+#Certo dia, o Prof. Humberto José Roberto fez o seguinte questionamento: se o zero à esquerda de um número não tem valor algum, 
+#por que teria em outras posições de um número? Portanto, ele pede sua ajuda para, ao somar dois valores inteiros, que o 
+#resultado seja exibido segundo o raciocínio dele, ou seja, sem os zeros. Por exemplo, ao somar 15 + 5, o resultado correto 
+#seria 20, mas com esta nova ideia, o novo resultado seria 2. Ao somar 99 + 6, o resultado correto seria 105, mas com esta nova ideia, 
+#o novo resultado seria 15. Escreva um programa que lê dois números inteiros (pode assumir que eles não têm o algarismo zero),
+#some os mesmos e, caso o resultado tenha algum algarismo zero, então os retire antes de imprimir na tela
+
 # -*- coding: utf-8 -*-
 
 num1 = int (input ('Digite o primeiro número: '))
