2b520cfd84642148a3e984360f2fbb0f653ba8c4
leticia-ribeiro98 <107193074+leticia-ribeiro98@users.noreply.github.com>
2022-06-09 11:40:22 -0300

    Create Problema 1.6.py

---
+++ Problema 1.6.py
@@ -0,0 +1,9 @@
+#Escreva um programa que leia dois números inteiros, e imprime na tela o resto da divisão do primeiro pelo segundo.
+
+
+# -*- coding: utf-8 -*-
+
+i1 = int (input('Digite o primeiro inteiro: '))
+i2 = int (input('Digite o segundo inteiro: '))
+resto = i1 % i2
+print ('Resto: %d' % resto)
