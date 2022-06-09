8caaf0d9a59f7454f77e958549d5a2a49d85f5ac
leticia-ribeiro98 <107193074+leticia-ribeiro98@users.noreply.github.com>
2022-06-09 11:43:58 -0300

    Create Problema 1.10.py

---
+++ Problema 1.10.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia três valores inteiros (a, b e c) e imprime a soma dos quadrados desses valores.
+
+# -*- coding: utf-8 -*-
+
+a = int (input('Digite o valor de a: '))
+b = int (input('Digite o valor de b: '))
+c = int (input('Digite o valor de c: '))
+cal = (a**2) + (b**2) + (c**2)
+print ('Resultado: %d' % cal)
