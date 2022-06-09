1908470d0894a287b3131eb8cfc220083581a9ed
leticia-ribeiro98 <107193074+leticia-ribeiro98@users.noreply.github.com>
2022-06-09 11:42:16 -0300

    Create Problema 1.8.py

---
+++ Problema 1.8.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia uma distância em milhas e apresente-a em quilômetros. Considere que:
+    
+# -*- coding: utf-8 -*-
+
+dist_m = float (input ('Digite a distância em milhas: '))
+convert_km = dist_m * 1.61
+print ('Distância em km: %.2f' % convert_km)
